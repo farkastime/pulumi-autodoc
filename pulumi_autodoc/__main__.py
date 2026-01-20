b5163df8ed4bdfe884464c303ca3f19b4f09fee6
farkastime <timothy.farkas@proton.me>
2026-01-20 12:09:18 -0700

    feat: add basic cli

---
+++ pulumi_autodoc/__main__.py
@@ -0,0 +1,16 @@
+from argparse import ArgumentParser
+
+
+def parse_args():
+    parser = ArgumentParser(description="Run the scaling pipeline with a config file.")
+    parser.add_argument("-R", "--remote", required=True, help="AWS S3 remote bucket.")
+    return parser.parse_args()
+
+
+def main(s3_remote: str):
+    pass
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(s3_remote=args.remote)
