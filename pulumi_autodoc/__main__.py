c0b58ba086b4cdf6ca2ae7a34081a9bbfc65f150
farkastime <timothy.farkas@proton.me>
2026-01-20 12:10:12 -0700

    feat: use URI not bucket name

--- pulumi_autodoc/__main__.py
+++ pulumi_autodoc/__main__.py
@@ -3,7 +3,7 @@ from argparse import ArgumentParser
 
 def parse_args():
     parser = ArgumentParser(description="Run the scaling pipeline with a config file.")
-    parser.add_argument("-R", "--remote", required=True, help="AWS S3 remote bucket.")
+    parser.add_argument("-R", "--remote", required=True, help="AWS S3 URI to stacks.")
     return parser.parse_args()
 
 
@@ -14,3 +14,4 @@ def main(s3_remote: str):
 if __name__ == "__main__":
     args = parse_args()
     main(s3_remote=args.remote)
+
